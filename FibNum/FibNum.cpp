3f57eb8b3f46a7d47027bcd33d313f7ab03766da
vartomic <varrava16@mail.ru>
2023-08-16 22:18:25 +0300

    Add project files.

---
+++ FibNum/FibNum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+struct Fib {
+
+    int size;
+
+    int &nums;
+
+    char sym;
+};
+
+//int createFib(Fib fib, int size, char sym) {
+//
+//    for (int i = 0; i <= size; i++) {
+//        return createFib(fib.nums - 1) + createFib(fib.nums - 2);
+//    }
+//    return 0;
+//}
+
+//void drawFib(Fib fib, int limit) {
+//
+//}
+
+int main() {
+
+    Fib fib;
+    //fib.size = 16;
+
+   // fib.sym = '*';
+
+    //createFib(fib, fib.size, fib.sym);
+    //drawFib(fib, 70);
+    return 0;
+}
+
+
