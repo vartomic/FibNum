e055c9387e68d1123e613f79262a32eba9f7a3fe
vartomic <varrava16@mail.ru>
2023-08-19 13:55:09 +0300

    19/08/23

--- FibNum/FibNum.cpp
+++ FibNum/FibNum.cpp
@@ -4,7 +4,7 @@ struct Fib {
 
     int size;
 
-    int nums[100];
+    int *nums;
 
     char sym;
 };
@@ -17,36 +17,43 @@ int createFib(Fib &fib, int size, char sym) {
     int num_next = 1;
     int num1 = 0;
     int num2 = 1;
+
+    fib.nums = new int[100];
     
     for (int i = 0; i <= size; i++) {
 
         fib.nums[i] = num_next;
-
+     
         num_next = num1 + num2;
-
         num1 = num2;
         num2 = num_next;
+
     }
     return 0;
 }
 
 void drawFib(Fib &fib, int limit) {
+
+    if (fib.size < limit) {
+        limit = fib.size;
+    }
+
     for (int i = 0; i < limit; i++) {
 
         int* ptr = &fib.nums[i];
 
-        std::cout << std::string(fib.nums[i], '*') << fib.nums[i] << std::endl;
+        std::cout << std::string(*ptr, '*') << *ptr << std::endl;
 
-        *ptr = fib.nums[i+1];
-    }
+        *ptr = fib.nums[i + 1];
+    }  
 }
 
 int main() {
 
     Fib fib;
 
-    createFib(fib, 16, '*');
-    drawFib(fib, 10);
+    createFib(fib, 7, '*');
+    drawFib(fib, 70);
     return 0;
 }
 
