a1a3e10ef810bfe628a549dbdb96730256f5e0df
vartomic <varrava16@mail.ru>
2023-08-21 21:58:06 +0300

    21/08/23

--- FibNum/FibNum.cpp
+++ FibNum/FibNum.cpp
@@ -12,16 +12,12 @@ struct Fib {
     char sym;
 };
 
-Fib* fabricFib(int size, char sym) {
-    Fib* fib = new struct Fib[size];
+Fib* createFib(Fib* fib, int size, char sym) {
+
     fib->size = size;
     fib->nums = new int[size];
     fib->sym = sym;
-    return fib;
-}
 
-int createFib(Fib &fib) {
-    
     //first number
     int num_next = 1;
 
@@ -30,13 +26,13 @@ int createFib(Fib &fib) {
 
     //third number
     int num2 = 1;
-    
+
     //loop continues while number of items in array < size of array
-    for (int i = 0; i <= fib.size; i++) {
+    for (int i = 0; i <= fib->size; i++) {
 
         //first item of array = 1
-        fib.nums[i] = num_next;
-     
+        fib->nums[i] = num_next;
+
         // next item of array is sum of next 2 numbers
         num_next = num1 + num2;
 
@@ -44,25 +40,36 @@ int createFib(Fib &fib) {
         num2 = num_next;
 
     }
-    return 0;
+    return fib;
 }
 
-void drawFib(Fib &fib, int limit) {
+Fib* fabricFib(int size, char sym) {
+
+    //allocate memory for fib
+    Fib* fib = new Fib[size];
+    
+    //fills fib
+    createFib(fib, size, sym);
+
+    return fib;
+}
+
+void drawFib(Fib *fib, int limit) {
     //if size of array lesser than limit, limit changes to equal to size of array
-    if (fib.size < limit) {
-        limit = fib.size;
+    if (fib->size < limit) {
+        limit = fib->size;
     }
     //loop continues while number of items in array < limit
     for (int i = 0; i < limit; i++) {
 
         //pointer to array
-        int* ptr = &fib.nums[i];
+        //int* ptr = &fib->nums[i];
 
         //prints '*' symbol as many times as current number in array[i] 
-        std::cout << std::string(*ptr, '*') << *ptr << std::endl;
+        std::cout << std::string(fib->nums[i], '*') << fib->nums[i] << std::endl;
 
         //pointer moves left on 1 position
-        *ptr = fib.nums[i + 1];
+        fib->nums[i + 1];
     }  
 }
 
@@ -70,8 +77,7 @@ int main() {
 
     Fib* fib = fabricFib(7, '*');
 
-    createFib(*fib);
-    drawFib(*fib, 70);
+    drawFib(fib, 70);
 
     return 0;
 }
